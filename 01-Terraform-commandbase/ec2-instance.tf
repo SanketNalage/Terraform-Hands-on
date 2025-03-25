ca4be8f9f8d384b9cb3fcdceeb15577f8821f640
Sanket_Nalage <sanketnalage88@gmail.com>
2025-03-26 01:58:06 +0530

    add the some file in he terraform

--- 01-Terraform-commandbase/ec2-instance.tf
+++ 01-Terraform-commandbase/ec2-instance.tf
@@ -1,5 +1,6 @@
 # Provider Block
 provider "aws" {
+  profile = "default"  
   region  = "eu-north-1"
 }
 
@@ -7,4 +8,7 @@ provider "aws" {
 resource "aws_instance" "ec2demo" {
   ami           = "ami-0c2e61fdcb5495691" # Amazon Linux in us-east-1, update as per your region
   instance_type = var.instance_type
+  tags = {
+    Name = "TerraformDemo"
+  }
 }
