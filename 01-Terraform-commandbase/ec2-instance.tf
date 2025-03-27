3c35017c81856eb70a25a09a12d303fba358d5f9
Sanket_Nalage <sanketnalage88@gmail.com>
2025-03-27 14:22:08 +0530

    add the some file in he terraform

--- 01-Terraform-commandbase/ec2-instance.tf
+++ 01-Terraform-commandbase/ec2-instance.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 # Resource Block
-resource "aws_instance" "ec2demo" {
+resource "aws_instance" "TerraformDemo" {
   ami           = "ami-0c2e61fdcb5495691"
   instance_type = var.instance_type
   tags = {
