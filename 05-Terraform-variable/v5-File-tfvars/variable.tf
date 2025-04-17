e4c24254c6493f3796948a9d684d5ffe1293bb2f
Sanket_Nalage <sanketnalage88@gmail.com>
2025-04-17 23:06:35 +0530

    Add the file and folder in project

---
+++ 05-Terraform-variable/v5-File-tfvars/variable.tf
@@ -0,0 +1,28 @@
+# There will be file name Terraform.tfvars file as it will have secuirty propuse variable declared in it.
+# for ex: if you have given the ec2_instance_count=1 in his file.
+# Then in the Terraform.tfvars it will override that value from this file as ec2_instance_count=2
+
+# Input Variables
+variable "aws_region" {
+  description = "Region in which AWS resources to be created"
+  type        = string
+  default     = "eu-north-1"
+}
+
+variable "ec2_ami_id" {
+  description = "AMI ID"
+  type        = string
+  default     = "ami-0274f4b62b6ae3bd5" # Amazon2 Linux AMI ID
+}
+
+variable "ec2_instance_count" {
+  description = "EC2 Instance Count"
+  type        = number
+  default     = 2
+}
+
+variable "ec2_type_instance" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"  # Fixed typo here
+}
