cf4f62c5af6e0b64bbe1bda86d483dade439c41c
Sanket_Nalage <sanketnalage88@gmail.com>
2025-04-18 23:33:52 +0530

    Added the new file and folder in project

--- 08-Terraform-Datasource/output.tf
+++ 08-Terraform-Datasource/output.tf
@@ -4,7 +4,7 @@ output "ec2_instance_publicip" {
   description = "EC2 Instance Public IP"
   value = aws_instance.my-ec2-vm.public_ip 
 }
-
+/*
 # Argument Reference: EC2 Instance Private IP
 output "ec2_instance_privateip" {
   description = "EC2 Instance Private IP"
@@ -15,7 +15,7 @@ output "ec2_security_groups" {
   description = "List Security Groups associated with EC2 Instance"
   value = aws_instance.my-ec2-vm.security_groups
 }
-
+*/
 # Attribute Reference - Create Public DNS URL with http:// appended
 output "ec2_publicdns" {
   description = "Public DNS URL of an EC2 Instance"
