2e09ee6836310015627350ba0f006fa602108167
Sanket_Nalage <sanketnalage88@gmail.com>
2025-04-21 16:51:18 +0530

    Added the new file and folder

--- 10-Terraform-Workspaces/10-1-local-backend/ec2-creation.tf
+++ 10-Terraform-Workspaces/10-1-local-backend/ec2-creation.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "my-ec2-vm" {
   ami           = data.aws_ami.amzlinux.id 
   instance_type = var.ec2_type_instance
   #instance_type = "t2.micro"
-  key_name      = "terraform-key"
+  key_name      = "terraform-pair"
   count = terraform.workspace == "default" ? 2 : 1 
 	user_data = file("apache-install.sh")  
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
