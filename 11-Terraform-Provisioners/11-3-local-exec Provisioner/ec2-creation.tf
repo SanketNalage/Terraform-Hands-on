38ff62f6290c196b0c079366138aa188453909de
Sanket_Nalage <sanketnalage88@gmail.com>
2025-04-23 12:17:15 +0530

    Add the file and folder in porject

---
+++ 11-Terraform-Provisioners/11-3-local-exec Provisioner/ec2-creation.tf
@@ -0,0 +1,31 @@
+# Create EC2 Instance - Amazon Linux
+resource "aws_instance" "my-ec2-vm" {
+  ami           = data.aws_ami.amzlinux.id 
+  instance_type = var.ec2_type_instance
+  #instance_type = "t2.micro"
+  key_name      = "terraform-pair"
+  count = terraform.workspace == "default" ? 2 : 1 
+	user_data = file("apache-install.sh")  
+  vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
+  tags = {
+    "Name" = "vm-${terraform.workspace}-0"
+  }
+
+  # local-exec provisioner (Creation-Time Provisioner - Triggered during Create Resource)
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.my-ec2-vm.private_ip} >> creation-time-private-ip.txt"
+    working_dir = "local-exec-output-files/"
+    #on_failure = continue
+  }
+
+  # local-exec provisioner - (Destroy-Time Provisioner - Triggered during Destroy Resource)
+  provisioner "local-exec" {
+    when    = destroy
+    command = "echo Destroy-time provisioner Instanace Destroyed at `date` >> destroy-time.txt"
+    working_dir = "local-exec-output-files/"
+  } 
+}
+
+
+
+
